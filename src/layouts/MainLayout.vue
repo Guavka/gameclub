d041b97b5b6ba24ea4cf66bc12d1e98ec8f68396
Алексей Мартынов <guavka@gmail.com>
2024-12-29 22:09:42 +0300

    create main menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,14 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
+  <q-layout view="lHh lpr lFf">
+    <q-footer>
+      <q-tabs>
+        <q-route-tab icon="home" to="/" exact />
+        <q-route-tab icon="shopping_cart" to="/order" exact />
+        <q-route-tab icon="qr_code_scanner" to="/qr" exact />
+        <q-route-tab icon="book_online" to="/booking" exact />
+        <q-route-tab icon="leaderboard" to="/leaders" exact />
+      </q-tabs>
+    </q-footer>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -15,3 +16,5 @@
 </template>
 
 <script setup lang="ts"></script>
+
+<style lang="css" scoped></style>
